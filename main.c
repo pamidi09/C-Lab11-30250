5a6f8083e13cda458fdc9d0d9c8aca0868c89248
A.P.N.Perera <132426742+pamidi09@users.noreply.github.com>
2023-07-31 13:58:51 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int product(int a, int b)
+void cal(int a,int b)
 {
-    int pro;
-    pro=a*b;
-
-    return pro;
+    int sum,diff;
+    sum=a+b;
+    diff=a-b;
+    printf("sum = %d\n",sum);
+    printf("difference = %d \n",diff);
 }
 
 int main()
 {
     int x,y;
+
     printf("Enter two numbers:");
     scanf("%d%d",&x,&y);
 
-    printf("the product is %d \n",product(x,y));
+    cal(x,y);
 }
