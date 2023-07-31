50349a38fe1a93cc5da876801648370b6ea32b71
A.P.N.Perera <132426742+pamidi09@users.noreply.github.com>
2023-07-31 14:03:27 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Q3
+
+int product(int a, int b)
+{
+    int pro;
+    pro=a*b;
+
+    return pro;
+}
+
+int main()
+{
+    int x,y;
+    printf("Enter two numbers:");
+    scanf("%d%d",&x,&y);
+
+    printf("the product is %d \n",product(x,y));
+}
+
+
+
+
+//Q2
+
 void cal(int a,int b)
 {
     int sum,diff;
@@ -18,4 +42,24 @@ int main()
     scanf("%d%d",&x,&y);
 
     cal(x,y);
+}
+
+
+
+
+//Q1
+void cal()
+{
+    int n1,n2,sum;
+
+    printf("Enter two numbers:");
+    scanf("%d%d",&n1,&n2);
+
+    (sum=n1+n2);
+    printf("sum = %d ",sum);
+}
+
+int main()
+{
+   cal();
 }
